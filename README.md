31ff55506965f9cfa8dfc5ffae97c4f62e3be8f0
eugeneshevchenko <shevchenko.e.a.ser@gmail.com>
2016-01-14 12:34:28 +0200

    first upload project

--- README.md
+++
@@ -1,3 +0,0 @@
-# singularis
-2D game created with awesome framework [libGDX](https://libgdx.badlogicgames.com/)
-
---
+++ desktop/src/org/shevalab/singularis/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.shevalab.singularis.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.shevalab.singularis.Singularis;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Singularis(), config);
+	}
+}
