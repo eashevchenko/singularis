8691d3a3be94a938d5dfa14ef58f6fc2a274fb84
eugeneshevchenko <shevchenko.e.a.ser@gmail.com>
2016-01-15 12:07:02 +0200

    added ImageResize util class

---
+++ desktop/src/org/shevalab/singularis/desktop/ImageResizer.java
@@ -0,0 +1,51 @@
+package org.shevalab.singularis.desktop;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Graphics2D;
+
+public class ImageResizer {
+
+    public static void resize(String inputImagePath,
+                              String outputImagePath, int scaledWidth, int scaledHeight)
+            throws IOException {
+
+        File inputFile = new File(inputImagePath);
+        BufferedImage inputImage = ImageIO.read(inputFile);
+
+        BufferedImage outputImage = new BufferedImage(scaledWidth,
+                scaledHeight, inputImage.getType());
+
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
+        g2d.dispose();
+
+        String formatName = outputImagePath.substring(outputImagePath
+                .lastIndexOf(".") + 1);
+
+        ImageIO.write(outputImage, formatName, new File(outputImagePath));
+    }
+
+    public static void main(String[] args) {
+
+        int scaledWidth = 128;
+        int scaledHeight = 128;
+
+        for (int i = 0; i < 10; i++) {
+            String inputImagePath = "in/Idle_00"+i+".png";
+            String outputImagePath1 = "out/Idle_00"+i+".png";
+            System.out.println(inputImagePath);
+            System.out.println(outputImagePath1);
+            try {
+
+                ImageResizer.resize(inputImagePath, outputImagePath1, scaledWidth, scaledHeight);
+            } catch (IOException ex) {
+                System.out.println("Error resizing the image.");
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
