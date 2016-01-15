0d6eb6778c7af37475deb98a103bcad5d6b4ebdc
eugeneshevchenko <shevchenko.e.a.ser@gmail.com>
2016-01-15 09:31:02 +0200

    added sprite and game camera. implemented projection matrix.

--- core/src/org/shevalab/singularis/Singularis.java
+++ core/src/org/shevalab/singularis/Singularis.java
@@ -3,25 +3,39 @@ package org.shevalab.singularis;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Singularis extends ApplicationAdapter {
+
+	float screenHeigth;
+	float screenWidth;
+
+	OrthographicCamera orthographicCamera;
+
 	SpriteBatch batch;
-	Texture img;
-	
+	Sprite sprite;
+
 	@Override
 	public void create () {
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeigth = Gdx.graphics.getHeight();
+		orthographicCamera = new OrthographicCamera(screenWidth, screenHeigth);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(new Texture("badlogic.jpg"));
+
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(orthographicCamera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(sprite,0,0);
 		batch.end();
 	}
 }
